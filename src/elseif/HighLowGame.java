bf996d50b727ef709e3862708652ca16d136f772
league <league@wts22.attlocal.net>
2017-12-29 17:08:30 -0800

    high low game, shiny

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,40 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(100) + 1;
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. do the following 10 times
-		
-			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+		for (int i = 0; i < 10; i++) {
+			// 1. ask the user for a guess using a pop-up window, and save their response
+			String guess = JOptionPane.showInputDialog("guess?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int guess2 = Integer.parseInt(guess);
 			// 5. if the guess is correct
-				// 6. win
-			// 7. if the guess is high
-				// 8. tell them it's too high
-			// 9. if the guess is low
-				// 10. tell them it's too low
+			if (guess2 == random) {
+				JOptionPane.showMessageDialog(null, "correct", "ERROR", JOptionPane.ERROR_MESSAGE);
+				ExceptionInInitializerError.class.getName().length();
+			} else if (guess2 < random) {
+				JOptionPane.showMessageDialog(null, "too low");
+			} else if (guess2 > random) {
+				JOptionPane.showMessageDialog(null, "too high");
+			}
+		}
+		JOptionPane.showMessageDialog(null, "you lose");
+
+		// 6. win
+		// 7. if the guess is high
+		// 8. tell them it's too high
+		// 9. if the guess is low
+		// 10. tell them it's too low
 
 		// 12. tell them they lose
 	}
 
 }
-
-
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,30 +5,32 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+		String n1 = JOptionPane.showInputDialog("Shiny objects #?");
 		// 3. Play the sound that many times
-
+		int n2 = Integer.parseInt(n1);
 		// 1. Call the method below
+		for (int i = 0; i < n2; i++) {
+			playMisterZee();
+		}
 
 	}
 
 	public static void playMisterZee() {
-    	try {
- 
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
-   		 clip.start();
-   		 Thread.sleep(3600);
-    	} catch (Exception ex) {
-        	ex.printStackTrace();
-    	}
-	}
+		try {
 
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
+			clip.start();
+			Thread.sleep(36);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
